3a626c97dac05417c4575c157087d139e73a277c
Dajanek Davis <>
2021-04-23 18:01:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+//Dajanek Davis
+// Football Scoreboard using 
+//Object Oriented Programming in C++
+//Updates 4-23-2021 - Dajanek Davis
+ 
+
+#include <iostream>
+#include <string> 
+#include <unistd.h>
+#include "Input_Validation_Extended.h"
+using namespace std; 
+
+class Team
+{
+  private:
+  string name;
+  string coach_name;
+  string home_city;
+  int score;
+  int timeout_count;
+  int shotsOnGoal; 
+  bool home_status;
+
+  public:
+      Team() //default constructor
+      {
+        score = 0;
+        home_status = false; // (visitor = false / home = true)
+        name = "DefaultTeamName";
+        shotsOnGoal = 0;
+        coach_name = "DefaultCoachName";
+        
+
+      }
+
+
+  void set_name(string n) { name = n; }
+  void set_coachName(string scN){coach_name =scN;}
+  void set_homeCity(string hC){ home_city = hC; }
+  void set_Score(int s) {score = s; }
+  void set_TimeoutCount(int tC){ timeout_count = tC; }
+ 
+
+
+}
